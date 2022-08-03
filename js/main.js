13156908fe42094fad7b9b0914cab68e47b4718e
Tapan Mandal <mandaltapan315@gmail.com>
2022-08-04 04:42:46 +0530

    js file modified extra code deleted

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
-let signIn = document.getElementsByClassName("sign-in-container");
-let signUp = document.getElementsByClassName("sign-up-container");
 
-let signInButton = document.getElementById('sign-in-btn');
-let signUpButton = document.getElementById('sign-up-btn');
-// console.log(signInButton);
-// console.log(signUpButton);
 
 function changeBg_one(){
     document.getElementById('in').style.backgroundColor = "var(--bg-sky)";
